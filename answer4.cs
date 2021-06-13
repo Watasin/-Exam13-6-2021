a3366bd9c8a14e7ff871cba7419324d23a5c30fe
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-06-13 17:16:00 +0700

    answer4

---
+++ answer4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exam_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Many City : ");
+
+
+            int Many_City = int.Parse(Console.ReadLine());
+
+
+            int Level = 0;
+
+            string text;
+            for (int i = 0; i < Many_City; i++)
+            {
+                Level.ToString();
+
+
+                Many_City City_loop;
+
+                City_loop = (Many_City)i;
+
+                Console.WriteLine("{0} = {1}", City_loop, Level);
+            }
+        }
+    }
+}
