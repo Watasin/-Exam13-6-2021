d734a68163f6b506a9c70471667bb3646a4b026c
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-06-13 16:43:20 +0700

    answer2

---
+++ answer2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exam_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input size of array : ");
+            int border = int.Parse(Console.ReadLine());
+            int[] ar1 = new int[border];
+            Console.WriteLine("This your size{0}", border);
+            for (int i = 0; i < border; i++)
+            {
+                Console.Write("amount - {0} ", i);
+                ar1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < border; i++)
+            { 
+                for(int j = i + 1; j < border; j++)
+                   {
+                   if (ar1[j] < ar1[i])
+                   {
+                        int tem = ar1[i];
+                        ar1[i] = ar1[j];
+                        ar1[j] = tem;
+
+                    }
+                }
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < border; i++)
+            {
+                Console.Write(ar1[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine ("\n\n");
+
+        }
+    }
+}
+        
+             
